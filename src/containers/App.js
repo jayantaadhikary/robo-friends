acf7e35c38b84812a6cdf6613cafc2b4955ba3e2
Jayanta Adhikary <73451350+jayantaadhikary@users.noreply.github.com>
2021-10-01 18:21:48 +0530

    added errorboundary

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll";
+import ErrorBoundary from "../components/ErrorBoundary";
 import "./App.css";
 
 class App extends Component {
@@ -22,11 +23,9 @@ class App extends Component {
   };
 
   render() {
-    const {robots, searchField} = this.state
+    const { robots, searchField } = this.state;
     const filteredRobots = robots.filter((robot) => {
-      return robot.name
-        .toLowerCase()
-        .includes(searchField.toLowerCase());
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
     if (robots.length === 0) {
       return <h1>Loading</h1>;
@@ -36,7 +35,9 @@ class App extends Component {
           <h1 className="f1">RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
           <Scroll>
-          <CardList robots={filteredRobots} />
+            <ErrorBoundary>
+              <CardList robots={filteredRobots} />
+            </ErrorBoundary>
           </Scroll>
         </div>
       );
